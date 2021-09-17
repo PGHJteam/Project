dc52e6c8678cdd39e897268674c8fc674cd3f7c2
zzisun <jisunkim98@gmail.com>
2021-09-17 10:08:08 +0900

    phpicker로 이미지 여러장 선택

---
+++ iOS/PGHJapp/Data/Network/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  PGHJapp
+//
+//  Created by 김지선 on 2021/09/17.
+//
+
+import Foundation
--- iOS/PGHJapp/PGHJapp/ViewController.swift
+++ iOS/PGHJapp/PGHJapp/ViewController.swift
@@ -1,19 +1,49 @@
-//
-//  ViewController.swift
-//  PGHJapp
-//
-//  Created by 김지선 on 2021/09/15.
-//
-
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func addButtonTouched(_ sender: Any) {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 0
+        configuration.filter = .any(of: [.images, .videos])
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configure()
     }
-
+    
+    func configure(){
+        addButton.layer.cornerRadius = 15
+    }
 
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        let itemProvider = results.first?.itemProvider
+
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                DispatchQueue.main.async {
+                    self.myImageView.image = image as? UIImage
+                }
+            }
+            
+        } else {
+            // error 처리
+        }
+
+    }
+    
+    
+}
