096653bb16af9e3bd9fc306c535b7fc59bb1cff4
apphia39 <apphia39@naver.com>
2021-10-05 02:57:48 +0900

    chore: serializers.py

--- BE/pghj_server/files/serializers.py
+++ BE/pghj_server/files/serializers.py
@@ -4,12 +4,12 @@ from files.models import Material
 
 class MaterialSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
+        print(validated_data)
         material = Material.objects.create(
             upload=validated_data['upload'],
             material=validated_data['material'],
             material_template=validated_data['material_template']
         )
-        
         return material
 
     class Meta:
