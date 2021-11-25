d480892267784435622514724cbb12eb1bd40036
zzisun <jisunkim98@gmail.com>
2021-11-25 14:21:01 +0900

    add: InitailViewController 생성

---
+++ iOS/PGHJapp/PGHJapp/Presentation/User/InitialViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InitialViewController.swift
+//  PGHJapp
+//
+//  Created by 김지선 on 2021/11/25.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
