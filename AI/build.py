c433c191e5c35e3bf13a25780f90ad21dc9f5535
hhzzang <86921751+hhzzang@users.noreply.github.com>
2021-11-27 02:03:23 +0900

    Update build.py

--- AI/build.py
+++ AI/build.py
@@ -1,4 +1,8 @@
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-from
+from kobert_transformers import get_tokenizer
+with import pickle
 eng_processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 eng_model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-handwritten")
+kobert = get_tokenizer()
+with open('kobert_processor.pkl','wb') as f:
+    pickle.dump(kobert,f)
