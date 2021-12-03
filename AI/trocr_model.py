f248432c239dd6e321188987ab43d0b1b855afe0
hhzzang <86921751+hhzzang@users.noreply.github.com>
2021-12-04 00:19:44 +0900

    Update trocr_model.py

--- AI/trocr_model.py
+++ AI/trocr_model.py
@@ -1,7 +1,15 @@
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import pickle
+with open('eng_processor.pkl','rb') as f:
+  eng_processor = pickle.load(f)
+with open('eng_htr_model.pkl','rb') as f:
+  eng_htr_model = pickle.load(f)
+with open('eng_ocr_model.pkl','rb') as f:
+  eng_ocr_model = pickle.load(f)
+  
 
-eng_processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
-eng_model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-handwritten")
+# eng_processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+# eng_model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-handwritten")
 
 # pixel_values = processor(image, return_tensors="pt").pixel_values
 # generated_ids = model.generate(pixel_values)
