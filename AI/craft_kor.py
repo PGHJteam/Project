f510a47f3d49f83cc460c1606fb9cbbb1f2390f6
hhzzang <86921751+hhzzang@users.noreply.github.com>
2021-12-04 02:38:15 +0900

    Update craft_kor.py

--- AI/craft_kor.py
+++ AI/craft_kor.py
@@ -262,24 +262,30 @@ def naver_recog(args,data,model,converter,img_size):
 
 def trocr_recog(dataset,recog_net,img_size,tr_task):
     one_image_res = []
-    for tr_sample,rect in dataset:
-        generated_ids = recog_net.generate(tr_sample)
-        text = dataset.processor.decode(generated_ids, skip_special_tokens=True)
-        total_x = img_size[1]
-        total_y = img_size[0]
-        x, y, w, h = rect
-        width = w / total_x
-        height = h / total_y
-        left = x / total_x
-        top = y / total_y
-        res_dict = dict()
-        res_dict['text'] = text
-        res_dict['coordinate'] = {"left":left,"top":top}
-        res_dict['size'] = {"width":width,"height":height}
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=int(args.workers), pin_memory=True)
+    recog_net.eval()
+    with torch.no_grad():
+        for tr_sample,rect in dataset:
+            generated_ids = recog_net.generate(tr_sample)
+            text = dataset.processor.decode(generated_ids, skip_special_tokens=True)
+            total_x = img_size[1]
+            total_y = img_size[0]
+            x, y, w, h = rect
+            width = w / total_x
+            height = h / total_y
+            left = x / total_x
+            top = y / total_y
+            res_dict = dict()
+            res_dict['text'] = text
+            res_dict['coordinate'] = {"left":left,"top":top}
+            res_dict['size'] = {"width":width,"height":height}
 
-        
-        res_dict['accuracy'] = 1.0
-        one_image_res.append(res_dict)
+
+            res_dict['accuracy'] = 1.0
+            one_image_res.append(res_dict)
     return one_image_res
 
 
