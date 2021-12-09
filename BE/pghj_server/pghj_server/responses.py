3ad257992f6337f4b3d7fdbbf9f4c5f7cf9b32fa
apphia39 <apphia39@naver.com>
2021-12-09 15:23:13 +0900

    chore

--- BE/pghj_server/pghj_server/responses.py
+++ BE/pghj_server/pghj_server/responses.py
@@ -1,5 +1,3 @@
-import json
-
 # Return Json Error Message
 def Error(error_message):
     response = {
@@ -11,32 +9,5 @@ def Error(error_message):
 def Success():
     response = {
         "detail": "Success."
-    }
-    return response
-
-# Return Json Response
-def JsonFormat(data, image_list, upload_id):
-    # Decode data
-    decoded_data = data.decode('cp949')  # utf-8
-
-    # Extract meaningful data from decoded data
-    results = decoded_data.split("\n") 
-    meaningful_data = results[-1]
-
-    # String to Dictionary
-    dict_result = json.loads(meaningful_data)
-    
-    # Make Json Format
-    images = []
-    for i in range(len(image_list)):
-        image = {
-            "image_id": i,
-            "results": dict_result[image_list[i]]
-        }
-        images.append(image)
-    
-    response = {
-        "upload_id": upload_id,
-        "images": images
     }
     return response
