1b149d82367dba285cef2941e4f5899238afb3fc
zzisun <jisunkim98@gmail.com>
2021-12-09 11:02:14 +0900

    add: 현재 화면에 대해 좌표 설정

--- iOS/PGHJapp/PGHJapp/Presentation/Lecture/ViewController/EditViewController.swift
+++ iOS/PGHJapp/PGHJapp/Presentation/Lecture/ViewController/EditViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Alamofire
+import SnapKit
 
 protocol EditDelegate {
     func sendEditedLecture(lecture: Lecture?)
@@ -18,24 +19,25 @@ class EditViewController: UIViewController {
     var lecture: Lecture?
     var page: Page?
     var delegate: EditDelegate?
+    private var fontSize: Int?
+    private var startPoint: CGPoint?
+    private var templateSize: CGSize?
     @IBOutlet weak var templateImageView: UIImageView!
+    @IBOutlet weak var fontSizeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
         configure()
+        configureCoordinates()
         
-        guard let pageID = pageID else { return }
-        guard let lecture = lecture else { return }
-        self.page = lecture.getPage(id: pageID)
-        print(page)
-        let editedPage = Page(id: pageID, sentences: [Sentence(sentence: "changed", coordinate: Coordinate(left: 0.34, top: 0.21), size: Size(height: 0.56, width: 0.45), font: Font(size: 10, type: "NanumBarunGothic"))])
-        self.page = editedPage
-        self.lecture?.editPage(id: pageID, page: page!)
-
-        let height = view.bounds.width - (navigationController?.navigationBar.bounds.height)! - 10.0
-        let width = height * 16/9
-        print(height, width)
+//        guard let pageID = pageID else { return }
+//        guard let lecture = lecture else { return }
+//        self.page = lecture.getPage(id: pageID)
+//        print(page)
+//        let editedPage = Page(id: pageID, sentences: [Sentence(sentence: "changed", coordinate: Coordinate(left: 0.34, top: 0.21), size: Size(height: 0.56, width: 0.45), font: Font(size: 10, type: "NanumBarunGothic"))])
+//        self.page = editedPage
+//        self.lecture?.editPage(id: pageID, page: page!)
     }
     
     private func configure() {
@@ -43,9 +45,16 @@ class EditViewController: UIViewController {
         configureBackground(templateID: lecture.templateID)
     }
     
-    func configureBackground(templateID: String?) {
+    private func configureBackground(templateID: String?) {
         let templateID = templateID ?? "" + ".png"
         templateImageView.image = UIImage(named: templateID)
+    }
+    
+    private func configureCoordinates() {
+        startPoint = CGPoint(x: 40.0, y: (navigationController?.navigationBar.bounds.height)!)
+        let height = view.bounds.width - (navigationController?.navigationBar.bounds.height)! - 10.0
+        let width = height * 16/9
+        templateSize = CGSize(width: width, height: height)
     }
     
     @IBAction func saveButtonTouched(_ sender: Any) {
